e592d0d5981cf323e8f6c2a7c404cc9e048862f0
Taimuraz Tibilov <tvtibilov@edu.hse.ru>
2020-05-11 14:18:21 +0300

    Ended with Documentation, sended on review

--- TaskManagerModel/src/org/taimuraztibilov/taskmanager/DataEditor.java
+++
@@ -1,9 +0,0 @@
-package org.taimuraztibilov.taskmanager;
-
-import java.sql.SQLException;
-
-public interface DataEditor {
-    void editData(String table, String column, String value, int id) throws SQLException;
-    void addLabelToTask(int labelId, int taskId) throws SQLException;
-    void removeLabelFromTask(int labelId, int taskId) throws SQLException;
-}
--- task_manager/src/main/java/org/taimuraztibilov/taskmanager/base/Main.java
+++
@@ -1,9 +0,0 @@
-package org.taimuraztibilov.taskmanager.base;
-
-import java.sql.SQLException;
-
-public class Main {
-    public static void main(String[] args) throws SQLException {
-        DataBaseManager manager = DataBaseManager.getInstance();
-    }
-}
